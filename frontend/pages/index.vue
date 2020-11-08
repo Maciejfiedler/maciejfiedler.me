9728b854bd69ec8353758b38e615950f2d31f36c
Maciej <maciek.fiedler@gmail.com>
2020-11-08 08:41:51 +0100

    Modified Website

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,15 +1,26 @@
 <template>
-  <c-box w="100vw" h="100vh" bg="blue.800" color="white"> </c-box>
+  <c-dark-mode>
+    <c-box p="20px" w="100vw" h="100vh" bg="blue.800" color="white">
+      <c-box p="20px" bg="blue.900" borderRadius="15px">
+        <c-stack spacing="2">
+          <c-heading as="h1" textAlign="center">Status:</c-heading>
+          <c-code fontSize="48px" textAlign="center">Good Good </c-code>
+        </c-stack>
+      </c-box>
+    </c-box></c-dark-mode
+  >
 </template>
 
-<script lang="js">
+<script lang="ts">
 /* eslint-disable vue/no-unused-components */
+
 import Chakra from '@chakra-ui/vue'
 
 export default {
   name: 'App',
   components: {
-    Chakra
+    Chakra,
   },
+  inject: ['$chakraColorMode', '$toggleColorMode'],
 }
 </script>
