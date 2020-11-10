7c371df7c3a80863437ed546746a9f36b96e0d4e
Maciej <maciek.fiedler@gmail.com>
2020-11-10 11:52:25 +0100

    Updated website(frontend)

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,47 +1,85 @@
 <template>
   <c-light-mode>
-    <c-box p="20px">
-      <c-box p="20px">
-        <c-stack spacing="2" pr="20px" pl="20px" textAlign="center">
-          <c-heading as="h1" size="2xl">Maciej Fiedler</c-heading>
+    <c-box p="20px" bg="blue.50">
+      <c-flex align="center" justify="center">
+        <c-box p="20px" maxW="1000px">
+          <c-stack spacing="2" pr="20px" pl="20px" textAlign="center">
+            <c-heading as="h1" size="2xl">Maciej Fiedler</c-heading>
+            <c-heading as="h2" size="xl">Status:</c-heading>
+            <c-code variant-color="blue" fontSize="40px">Good </c-code>
+            <!-- inplement API request here -->
+            <br />
+            <c-heading as="h2" size="xl">Interests:</c-heading>
+            <c-text fontSize="xl"
+              >Linux, backend systems, astronomy, physics, music, Overwatch,
+              computer science, building websites, Minecraft, learning new
+              programming languages and technologies, Tennis, Ju-Jitsu
+            </c-text>
+            <!-- inplement API request here -->
+          </c-stack>
           <br />
-          <c-heading as="h2" size="xl">Status:</c-heading>
-          <c-code fontSize="40px">Good </c-code>
-          <!---inplement API request here --->
-          <br />
-          <c-heading as="h3" size="xl">Interests:</c-heading>
-          <c-heading as="h4" size="lg"
-            >Linux, backend systems, astronomy, physics, music, Overwatch,
-            computer science, building websites, Minecraft, learning new
-            programming languages and technologies
-          </c-heading>
-          <!---inplement API request here --->
-        </c-stack>
-        <br />
-        <c-flex align="center" justify="center">
-          <c-link
-            bg="black"
-            color="white"
+          <c-heading as="h2" size="xl" textAlign="center">Social</c-heading>
+          <c-stack
+            spacing="2"
+            pr="20px"
+            pl="20px"
             textAlign="center"
-            role="Lead to Github Repository"
-            is-external
-            p="16px"
-            rounded="5px"
-            href="https://github.com/Maciejfiedler/maciejfiedler.me"
-            >Github Repository</c-link
+            justify="center"
+            isInline
           >
-        </c-flex>
+            <c-link
+              textAlign="center"
+              is-external
+              fontSize="lg"
+              href="https://twitter.com/MaciejFiedlerOn"
+              color="blue.400"
+              role="Lead to Twitter Account"
+              >Twitter</c-link
+            >
+            <c-link
+              textAlign="center"
+              is-external
+              fontSize="lg"
+              href="https://www.instagram.com/maciej.me/"
+              color="pink.400"
+              role="Lead to Instagram Account"
+              >Instagramm</c-link
+            >
+            <c-link
+              textAlign="center"
+              is-external
+              role="Lead to Github Account"
+              fontSize="lg"
+              href="https://github.com/Maciejfiedler"
+              color="black"
+              >Github</c-link
+            >
+          </c-stack>
+          <br />
+          <c-flex align="center" justify="center">
+            <c-link
+              bg="black"
+              color="white"
+              textAlign="center"
+              role="Lead to Github Repository"
+              is-external
+              p="8px"
+              rounded="5px"
+              href="https://github.com/Maciejfiedler/maciejfiedler.me"
+              >Github Repository</c-link
+            >
+          </c-flex>
 
-        <c-image
-          p="20px"
-          w="600px"
-          mr="auto"
-          ml="auto"
-          src="https://raw.githubusercontent.com/Maciejfiedler/maciejfiedler.me/main/Assets/Logo%20(high%20resolution).png"
-          alt="Logo, a duck on a star background"
-          role="Show the Logo of the page"
-        />
-      </c-box> </c-box
+          <c-image
+            p="20px"
+            w="600px"
+            mr="auto"
+            ml="auto"
+            src="https://raw.githubusercontent.com/Maciejfiedler/maciejfiedler.me/main/Assets/Logo%20(high%20resolution).png"
+            alt="Logo, a duck on a star background"
+            role="Show the Logo of the page"
+          /> </c-box
+      ></c-flex> </c-box
   ></c-light-mode>
 </template>
 
