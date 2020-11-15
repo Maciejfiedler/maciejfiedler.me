4f3250a68bc8e2f83ab6dc1c4805a29a95fc76f9
Maciejfiedler <maciek.fiedler@gmail.com>
2020-11-15 20:51:52 +0100

    Database and API work now

--- backend/app/database.py
+++ backend/app/database.py
@@ -4,13 +4,16 @@ r = redis.Redis(host='172.26.0.104', port=6379,
                 decode_responses=True)
 
 
-def __init__():
+def check_if_keys_exist():
     if r.exists("my_status") == False:
         r.set("my_status", "Good")
 
-    elif r.exists("my_description") == False:
+    if r.exists("my_description") == False:
         r.set("my_description",
               "My name is Maciej Fiedler. I am 15 years old and i like to programm. Besides programming I also do Ju-Jitsu, play Tennis andVideogames.")
 
-    elif r.exists("my_interests") == False:
+    if r.exists("my_interests") == False:
         r.set("my_interests", "Backend Systems, Overwatch, Tennis, Physics, Ju-Jitsu, Design,Linux, Music, PCs, Minecraft, Learning Programming languages and technologies, New technologies(RISC-V, Quantum Computing etc.)")
+
+
+check_if_keys_exist()
