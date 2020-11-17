ac166ab9b318453447ae8e7ab2b6fdc6e38f4707
Maciejfiedler <maciek.fiedler@gmail.com>
2020-11-17 13:13:55 +0100

    Refactor

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,12 +4,8 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
-    # Admin
-    from . import admin
-    app.register_blueprint(admin.bp)
-
     # API
-    #from . import api
-    # app.register_blueprint(api.bp)
+    from . import api
+    app.register_blueprint(api.bp)
 
     return app
