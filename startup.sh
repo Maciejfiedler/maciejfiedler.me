3e47474a7410c1e498e9c1f3bc0265fcfd8becd8
Maciej Fiedler <54156166+Maciejfiedler@users.noreply.github.com>
2020-12-02 15:16:20 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 # This file is optional and uses screen to create a terminal in a seperate window for this service
 screen -dmS maciejfiedler.me-website
 screen -S maciejfiedler.me-website -X stuff $'cd /home/maciejfieler/maciejfiedler.me/ \n'
-screen -S maciejfiedler.me-website -X stuff $'./prod_start.sh \n'
+screen -S maciejfiedler.me-website -X stuff $'./prod_changes.sh \n'
